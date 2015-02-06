caf2a9221bbba2d159e2b6a8f2812c375c885c43
TobinH <TobinH@users.noreply.github.com>
2015-02-06 18:51:53 -0500

    time-annotated version
    for tracking computation time of separate processes

--- gen.sp.eignf.R
+++ gen.sp.eignf.R
@@ -3,20 +3,17 @@ gen.sp.eignf<-function(x,cortical=FALSE) {
   require(vegan)
   if(cortical) {
     posdist<-dist(x,method="manhattan")
-    print("Distance matrix completed at "Sys.time())
+    print(paste("Distance matrix completed at",Sys.time()))
     posdist[which(posdist>1)]<-4
     sp.eigenf<-pcoa(posdist)
-    print("PCoA completed at "Sys.time())
+    print(paste("PCoA completed at",Sys.time()))
   } else {
     posdist<-dist(x,method="euclidean")
-    print("Distance matrix completed at "Sys.time())
+    print(paste("Distance matrix completed at",Sys.time()))
     postree<-spantree(posdist)
-    print("Minimum spanning tree completed at "Sys.time())
+    print(paste("Minimum spanning tree completed at",Sys.time()))
     posmax<-max(postree$dist)
     posdist[which(posdist>posmax)]<-posmax*4
     sp.eigenf<-pcoa(posdist)
-    print("PCoA completed at "Sys.time())
+    print(paste("PCoA completed at",ys.time()))
   }
-  result<-sp.eigenf$vectors
-  return(result)
-}
