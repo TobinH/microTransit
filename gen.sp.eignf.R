0783672ba5f0eeb7625fcc964733959db48d2d71
TobinH <TobinH@users.noreply.github.com>
2015-02-08 21:09:46 -0500

    Update gen.sp.eignf.R
    commented strings for parallelized matrix centering function--don't know if this would be faster or not.

--- gen.sp.eignf.R
+++ gen.sp.eignf.R
@@ -1,12 +1,10 @@
-gen.sp.eignf<-function(x,cortical=FALSE) {
+gen.sp.dist<-function(x,cortical=FALSE) {
   require(ape)
   require(vegan)
   if(cortical) {
     posdist<-dist(x,method="manhattan")
     print(paste("Distance matrix completed at",Sys.time()))
     posdist[which(posdist>1)]<-4
-    sp.eigenf<-pcoa(posdist)
-    print(paste("PCoA completed at",Sys.time()))
   } else {
     posdist<-dist(x,method="euclidean")
     print(paste("Distance matrix completed at",Sys.time()))
@@ -14,6 +12,40 @@ gen.sp.eignf<-function(x,cortical=FALSE) {
     print(paste("Minimum spanning tree completed at",Sys.time()))
     posmax<-max(postree$dist)
     posdist[which(posdist>posmax)]<-posmax*4
-    sp.eigenf<-pcoa(posdist)
-    print(paste("PCoA completed at",Sys.time()))
   }
+  centre <- function(D, n) {
+    One <- matrix(1, n, n)
+    mat <- diag(n) - One/n
+    mat.cen <- mat %*% D %*% mat
+  }
+  D<-as.matrix(posdist)
+  n<-nrow(D)
+  epsilon<-sqrt(.Machine$double.eps)
+  delta1<-centre((-0.5*D^2),n)
+  trace<-sum(diag(delta1))
+  D.eig<-eigen(delta1)
+  #min.eig<-min(D.eig$values)
+  eig<-(D.eig$values)
+  k<-length(which(eig>epsilon))
+  #rel.eig<-eig[1:k]/trace
+  #cum.eig<-cumsum(rel.eig)
+  results<-sweep(D.eig$vectors[,1:k],2,sqrt(eig[1:k]),FUN="*")
+  return(results)
+}
+
+#centre <- function(D, n) {
+#  require(snow)
+#  One <- matrix(1, n, n)
+#  mat <- diag(n) - One/n
+#  mat.cen <- parMM(cl,mat,(parMM(cl,D,mat)))
+#}
+
+
+#library(snow)
+#library(parallel)
+#library(doParallel)
+#cl<-makeCluster(detectCores(),outfile="")
+#registerDoParallel(cl)
+
+
+#stopCluster(cl)
