c1b0bbabe12f82c11d2322c1098b71b24f0e69e9
TobinH <TobinH@users.noreply.github.com>
2015-02-10 22:55:37 -0500

    Update dbMEM.model.R
    tested to 30 eigenfunctions (~12 min run time on Avizo machine)--rashly tried to run full vector set (11593 var?) and manually stopped before fit traces started printing.

--- dbMEM.model.R
+++ dbMEM.model.R
@@ -1,16 +1,23 @@
 # dbMEM analysis function
 # using spatial eigenfunctions from gen.sp.eigenfunction
 
-dbMEM.model<-function(xyz,MEM){
-  require(MASS)
-  xnam <- paste0("x", 1:ncol(MEM))
-  colnames(MEM)<-xnam
+dbMEM.model<-function(qPLMtab,MEM){
+  require(qtlmt)
+  xnam <- paste("x_", 1:ncol(MEM),sep="")
   xyznam<-c("x","y","z")
+  xyz<-matrix(data=qPLMtab$pixels[,3:5],ncol=3)
   colnames(xyz)<-xyznam
-  fmlaup<-as.formula(paste(paste(xyznam,collapse= "+")," ~ ", paste(xnam, collapse= "+")))
-  fmlalo<-as.formula(paste(paste(xyznam,collapse="+"),"~ 1"))                     
-  fitupper<-lm(fmlaup)
-  fitlower<-lm(fmlalo)
-  results<-stepAIC(fit2,direction="forward",scope=list(upper=fit1,lower=fit2))
+  one<-matrix(data=1,nrow=nrow(xyz),ncol=1)
+  colnames(one)<-"one"
+  pixindex<-match(as.data.frame(t(qPLMtab$pixels[,8:9])),as.data.frame(t(qPLMtab$distance)))
+  MEMpix<-matrix(data=0,nrow=nrow(xyz),ncol=ncol(MEM))
+  MEMpix<-MEM[pixindex,]
+  colnames(MEMpix)<-xnam
+  eignf.data<<-as.data.frame(cbind(xyz,one,MEMpix))
+  fmlaup<-as.formula(paste("cbind(x,y,z) ~ one + ", paste(xnam, collapse= "+")))
+  fmlalo<-as.formula(cbind(x,y,z) ~ one)                    
+  fitupper<-lm(fmlaup,data=eignf.data)
+  fitlower<-lm(fmlalo,data=eignf.data)
+  results<-mStep(fitlower,direction="forward",scope=list(upper=fitupper,lower=fitlower),trace=TRUE)
   return(results)
 }
