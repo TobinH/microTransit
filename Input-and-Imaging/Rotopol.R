ba93b7537735c8067cdb9ea02cf079eefbc24502
TobinH <TobinH@users.noreply.github.com>
2015-02-18 19:33:27 -0500

    Create Rotopol.R
    files for fast composite scripting

---
+++ Input-and-Imaging/Rotopol.R
@@ -0,0 +1,15 @@
+# prompted wrapper for Rotopol.qPLM.R, meant to be run as a batch file from Windows
+
+Rotopol<-function(){
+  sample.name<-readline("Name the sample:")
+  thickness<-as.numeric(readline("Specimen thickness (in microns):"))
+  wavelength<-as.numeric(readline("Illumination wavelength (in nm--hit ENTER for default of 532):"))
+  if (is.na(wavelength)) {
+    wavelength<-532
+  }
+  birefringence<-as.numeric(readline("Birefringence of tissue (hit ENTER for extant bone default of 0.005):"))
+  if (is.na(birefringence)) {
+    birefringence<-0.005
+  }
+  Rotopol.qPLM(sample.name=sample.name,thickness=thickness,wavelength=wavelength,birefringence=birefringence,mask=TRUE, comp.bkgrnd=100)
+}
