5cbfa4632160515a059c8978c663308667ded659
TobinH <h.tobin@gmail.com>
2015-02-18 20:01:59 -0500

    Easy-button update

--- Input-and-Imaging/Easy.button.R
+++ Input-and-Imaging/Easy.button.R
@@ -1,3 +1,4 @@
-source('~/GitHub/microtransit/Rotopol.qPLM.R')
-source('~/GitHub/microtransit/Rotopol.R')
+source('~/GitHub/microtransit/Input-and-Imaging/Rotopol.qPLM.R')
+source('~/GitHub/microtransit/Input-and-Imaging/Rotopol.R')
+setwd(choose.dir(caption="Select Location to Save Composite Images"))
 Rotopol()
