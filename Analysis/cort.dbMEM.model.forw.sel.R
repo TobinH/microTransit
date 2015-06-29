1a2d88647ebe4c923e9e4cc3725f2bd688bdbc68
TobinH <h.tobin@gmail.com>
2015-06-29 10:45:38 -0400

    forward_to_the_past
    forward selection of spatial eigenfunctions for cortical modeling of
    collagen fiber orientation

--- Analysis/cort.dbMEM.model.forw.sel.R
+++ Analysis/cort.dbMEM.model.forw.sel.R
@@ -8,14 +8,16 @@
 cort.dbMEM.model<-function(data,data.pos,MEM,penalty=2){
   require(qtlmt)
   pos.index<-match(data.frame(t(data.pos)), data.frame(t(MEM[,1:2])))
-  
-  eignf.data<-as.data.frame(cbind(data,MEM[pos.index,]))
-  colnames(eignf.data[,1:4])<-c("st","ct","sp","cp")
-  colnames(eignf.data[,5:ncol(MEM)+2])<-colnames(MEM[,3:ncol(MEM)])
-  fmlaup<-as.formula(paste("cbind(st,ct,sp,cp) ~ one + ", paste(fnam, collapse= "+")))
-  fmlalo<-as.formula(cbind(st,ct,sp,cp) ~ one)                    
+  # match eigenvectors to radial and centrifugal positions of pixel data
+  eignf.data<<-as.data.frame(cbind(data,MEM[pos.index,3:ncol(MEM)]))
+  # compose analysis data frame
+  print(dim(eignf.data))
+  print(colnames(eignf.data)[1:5])
+  fmlaup<-as.formula(paste("cbind(sin_theta_doubled,cos_theta_doubled,sin_phi_corr,cos_phi_corr) ~ 1 + ", paste(colnames(MEM)[3:ncol(MEM)], collapse= "+")))
+  fmlalo<-as.formula(cbind(sin_theta_doubled,cos_theta_doubled,sin_phi_corr,cos_phi_corr) ~ 1)                    
   fitupper<-lm(fmlaup,data=eignf.data)
   fitlower<-lm(fmlalo,data=eignf.data)
   results<-mStep(fitlower,direction="forward",scope=list(upper=fitupper,lower=fitlower),trace=TRUE,k=penalty)
+  rm(eignf.data)
   return(results)
 }
