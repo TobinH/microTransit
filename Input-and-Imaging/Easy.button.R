27783c2f453afd7aaea8f127c937af154c3be79e
TobinH <h.tobin@gmail.com>
2018-02-26 13:42:02 -0500

    yakkity_yak
    Removing outdated files

--- Input-and-Imaging/Easy.button.R
+++
@@ -1,4 +0,0 @@
-source('~/GitHub/microtransit/Input-and-Imaging/Rotopol.qPLM.R')
-source('~/GitHub/microtransit/Input-and-Imaging/Rotopol.R')
-setwd(choose.dir(caption="Select Location to Save Composite Images"))
-Rotopol()
