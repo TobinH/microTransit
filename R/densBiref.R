e88a4fe426106c3e026b288e2b2271141528509e
TobinH <TobinH@users.noreply.github.com>
2023-11-09 17:11:00 -0500

    Update densBiref.R

--- R/densBiref.R
+++ R/densBiref.R
@@ -1,8 +1,13 @@
 #' @title Estimate Birefringence of Bone Tissue From Mineral Density
 #'
-#' @description \code{densBiref} uses a naive analytical approach to estimating
-#' birefringence from bone mineral density, as expressed in "milligrams of
-#' hydroxyapatite per cubic centimeter" (mgHA/ccm).
+#' @description \code{densBiref} uses a simple analytical approach to estimating
+#'   birefringence from bone mineral density, as expressed in "milligrams of
+#'   hydroxyapatite per cubic centimeter" (mgHA/ccm).
+#'
+#' @details The actual relationship between bone mineral density and birefringence
+#'   is complicated, because much of the birefringence of collagen is form birefringence
+#'   (Dallemagne and Mélon 1946; Ascenzi and Bonucci 1961), but this approach provides a
+#'   useful heuristic.
 #'
 #' @param density estimate of mineral density in the sample, in units of
 #'   mgHA/ccm.
@@ -13,7 +18,15 @@
 #'
 #' @param birefC birefringence of collagen. Default is 8.933e-3.
 #'
-#' @return numeric value giving the estimated birefringence.
+#' @return Numeric value giving the estimated birefringence.
+#'
+#' @references Ascenzi, A., & Bonucci, E. (1961). A quantitative investigation of the
+#'   birefringence of the osteon. \emph{Acta Anatomica}, 44(3), 236-262.
+#'
+#'   Dallemagne, M. J., & Melon, J. (1946). Nouvelles recherches relatives aux propriétés
+#'   optiques de l'os: La biréfringence de l'os minéralisé; Relations entre les fractions
+#'   organiques et inorganiques de l'os. \emph{Journal of the Washington Academy of Sciences},
+#'   36(6), 181-195.
 #'
 #' @family qPLM Input Functions
 #'
